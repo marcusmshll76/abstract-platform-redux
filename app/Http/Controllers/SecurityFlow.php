2c84aea4783bab101e0fee7888969b12bd73d850
Phil Crumm <pkcrumm@gmail.com>
2019-05-23 17:27:03 -0700

    Adjust titles for CDS pages

--- app/Http/Controllers/SecurityFlow.php
+++ app/Http/Controllers/SecurityFlow.php
@@ -11,47 +11,47 @@ use App\Http\Controllers\Controller;
 class SecurityFlow extends Controller
 {
     public function choose () {
-        return view( 'security-flow.step-1.choose', [ 'title' => 'Security Flow -> Choose Digital Securities' ] );
+        return view( 'security-flow.step-1.choose', [ 'title' => 'Create Digital Security > Security Type' ] );
     }
 
     public function upload () {
-        return view( 'security-flow.step-1.upload', [ 'title' => 'Security Flow -> Upload Photos' ] );
+        return view( 'security-flow.step-1.upload', [ 'title' => 'Create Digital Security > Upload Photos' ] );
     }
 
     public function details (Request $request) {
-        return view( 'security-flow.step-1.details', [ 'title' => 'Security Flow -> Upload Photos' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-1.details', [ 'title' => 'Create Digital Security > Upload Photos' ] )->with('data', $request->session()->get('security-flow'));
     }
     
     public function highlights (Request $request) {
-        return view( 'security-flow.step-1.highlights', [ 'title' => 'Security Flow -> Highlights' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-1.highlights', [ 'title' => 'Create Digital Security > Highlights' ] )->with('data', $request->session()->get('security-flow'));
     }
 
     public function ownership (Request $request) {
-        return view( 'security-flow.step-2.ownership', [ 'title' => 'Security Flow -> Ownership' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-2.ownership', [ 'title' => 'Create Digital Security > Ownership' ] )->with('data', $request->session()->get('security-flow'));
     }
 
     public function diligence () {
-        return view( 'security-flow.step-3.diligence', [ 'title' => 'Security Flow -> Ownership' ] );
+        return view( 'security-flow.step-3.diligence', [ 'title' => 'Create Digital Security > Ownership' ] );
     }
 
     public function keyPoints (Request $request) {
-        return view( 'security-flow.step-4.key-points', [ 'title' => 'Security Flow -> Key Points' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-4.key-points', [ 'title' => 'Create Digital Security > Key Points' ] )->with('data', $request->session()->get('security-flow'));
     }
 
     public function capitalStack (Request $request) {
-        return view( 'security-flow.step-5.capital-stack', [ 'title' => 'Security Flow -> Capital Stack' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-5.capital-stack', [ 'title' => 'Create Digital Security > Capital Stack' ] )->with('data', $request->session()->get('security-flow'));
     }
 
     public function meetSponsors (Request $request) {
-        return view( 'security-flow.step-6.meet-sponsors', [ 'title' => 'Security Flow -> Meet the Sponsors' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-6.meet-sponsors', [ 'title' => 'Create Digital Security > Meet the Sponsors' ] )->with('data', $request->session()->get('security-flow'));
     }
 
     public function preview (Request $request) {
-        return view( 'security-flow.step-7.preview', [ 'title' => 'Security Flow -> Preview' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-7.preview', [ 'title' => 'Create Digital Security > Preview' ] )->with('data', $request->session()->get('security-flow'));
     }
 
     public function final (Request $request) {
-        return view( 'security-flow.step-7.final', [ 'title' => 'Security Flow -> Preview & Submit' ] )->with('data', $request->session()->get('security-flow'));
+        return view( 'security-flow.step-7.final', [ 'title' => 'Create Digital Security > Preview & Submit' ] )->with('data', $request->session()->get('security-flow'));
     }
 
     // Save Data into a session
