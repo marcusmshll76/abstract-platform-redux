04c5b569a365e40461971f389e73217a9e6bd45f
Phil Crumm <pkcrumm@gmail.com>
2019-06-18 12:41:35 -0700

    Update box API getURL function

--- resources/js/libs/index.js
+++ resources/js/libs/index.js
@@ -1,7 +1,11 @@
 function getApiUrl() {
 	let hn = window.location.hostname;
-	return 'http://' + hn + '/';
-	
+	if( hn === 'abstract.inc.treyventures.com' ) {
+		return 'http://abstract.inc.treyventures.com/';
+	} else {
+		return 'http://acg.abstract.inc.treyventures.com/';
+	}
+
 	if (hn === 'abstract.test') {
 		return 'http://abstract.test/'
 	} else {
