1c8b4f235bcd6ac6f136de963cb3eebbae8307e4
Phil Crumm <pkcrumm@gmail.com>
2019-06-21 18:09:35 -0700

    Send emails when onboarding an investor.

--- app/CapTableHelper.php
+++ app/CapTableHelper.php
@@ -7,8 +7,9 @@ class CapTableHelper {
         
         foreach( $cap_table as $investor ) {
             // Determine if the investor is already a user on our platform.
-            $maybe_user = DB::table( 'users' )->select( 'id' )->where( 'email', $investor['email'] )->first();
-            
+            $maybe_user = DB::table( 'users' )->where( 'email', $investor['email'] )->first();
+            $new_user = false;
+
             if( !$maybe_user ) {
                 // Create the user and invite them.
                 DB::table( 'users' )->insert( [
@@ -19,9 +20,8 @@ class CapTableHelper {
                     'type'          => 'investor'
                 ] );
 
-                // @TODO send welcome email
-
-                $maybe_user = DB::table( 'users' )->select( 'id' )->where( 'email', $investor['email'] )->first();
+                $maybe_user = DB::table( 'users' )->where( 'email', $investor['email'] )->first();
+                $new_user = true;
             }
 
             // Associate the user with the property.
@@ -34,6 +34,41 @@ class CapTableHelper {
                 'share'             => $investor['stake'],
                 'type'              => 'sproperty', // @TODO make dynamic
             ] );
+
+            $site = DB::table( 'sites' )->where( 'id', 2 )->first();
+            $invite_link = 'http://' . $site->host . '/invite/' . $maybe_user->invite_code;
+            $property = DB::table( 'property' )->where( 'id', $property_id )->first();
+            $subject = $property->opportunity_name . ' Is Now Serviced By Abstract Tokenization';
+
+            // Send the investor an email
+            if( $new_user ) {
+                $message = <<<EOD
+{$property->opportunity_name}, a portfolio property owned by {$investor['entity_name']} is now serviced by the Abstract Tokenization platform! With Abstract, you can efficiently view reports, download tax forms, and more.
+
+To get started, create your account by clicking this link:
+$invite_link
+
+-The Abstract Tokenization Team
+EOD;
+            } else {
+                $invite_link = 'http://' . $site->host . '/login';
+                $message = <<<EOD
+{$property->opportunity_name}, a portfolio property owned by {$investor['entity_name']} is now serviced by the Abstract Tokenization platform!
+
+Login to your account to view your reports:
+$invite_link
+
+-The Abstract Tokenization Team
+EOD;
+            }
+
+            $email = new \SendGrid\Mail\Mail(); 
+            $email->setFrom( 'no-reply@abstracttokenization.com', 'Abstract Tokenization' );
+            $email->setSubject( $subject );
+            $email->addTo( $investor['email'] );
+            $email->addContent( 'text/plain', $message );
+            $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+            $sendgrid->send( $email );
         }
 
         return true;
