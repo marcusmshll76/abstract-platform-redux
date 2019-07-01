e6bd307332e2baf248e99bc04b485aaef2070be0
Benedict Asamoah <benedict@sturta.com>
2019-07-01 23:48:45 +0000

    files

---
+++ database/migrations/2019_07_01_234345_files.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Files extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user')->nullable();
+            $table->string('map')->nullable();
+            $table->string('name')->nullable();
+            $table->string('field')->nullable();
+            $table->string('path')->nullable();
+            $table->string('status')->default('unverified');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
