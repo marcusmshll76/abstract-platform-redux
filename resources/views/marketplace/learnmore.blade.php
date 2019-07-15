94a3058ebe8a7ff46ef9bed1bb7516dd750dfb57
Benedict Asamoah <wrilben@icloud.com>
2019-07-15 16:31:49 +0000

    Marketplace Grid fixed

--- resources/views/marketplace/learnmore.blade.php
+++ resources/views/marketplace/learnmore.blade.php
@@ -111,64 +111,9 @@
                         </div>
                     </div>
                 </div>     
-                            @endforeach
-                            @endif
-                
-                <div class="col-xs-12 col-sm-4">
-                    <div class="card">
-                        <div class="marketplace-card-image porperty-image">
-                            <div class="marketplace-card-image-description">
-                                <h5>Mullbery Place Portland, OR</h5>
-                                <p>Mixed Family</p>
-                            </div><img src="/img/img-demo-2.jpg"></div>
-                        <div class="marketplace-card-info">
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Targeted Investor IRR</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>22.8%</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Tareted Equity Multiple </p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>1.8x</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Targeted Invesetment Period</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>3yrs</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Minimum Investment</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>$25,000</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Project Type</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>MultiFamily</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="marketplace-card-button">
-                            <div class="btn full-width">Learn More</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-4">
+            @endforeach
+        @endif
+        <div class="col-xs-12 col-sm-4 margin-top-m">
                     <div class="card">
                         <div class="marketplace-card-image porperty-image">
                             <div class="marketplace-card-image-description">
@@ -222,9 +167,169 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row margin-top-m">
-                <div class="col-xs-12 col-sm-4">
+                <div class="col-xs-12 col-sm-4 margin-top-m">
+                    <div class="card">
+                        <div class="marketplace-card-image porperty-image">
+                            <div class="marketplace-card-image-description">
+                                <h5>Mullbery Place Portland, OR</h5>
+                                <p>Mixed Family</p>
+                            </div><img src="/img/img-demo-2.jpg"></div>
+                        <div class="marketplace-card-info">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Targeted Investor IRR</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>22.8%</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Tareted Equity Multiple </p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>1.8x</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Targeted Invesetment Period</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>3yrs</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Minimum Investment</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>$25,000</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Project Type</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>MultiFamily</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="marketplace-card-button">
+                            <div class="btn full-width">Learn More</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4 margin-top-m">
+                    <div class="card">
+                        <div class="marketplace-card-image porperty-image">
+                            <div class="marketplace-card-image-description">
+                                <h5>Mullbery Place Portland, OR</h5>
+                                <p>Mixed Family</p>
+                            </div><img src="/img/img-demo-3.jpg"></div>
+                        <div class="marketplace-card-info">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Targeted Investor IRR</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>22.8%</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Tareted Equity Multiple </p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>1.8x</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Targeted Invesetment Period</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>3yrs</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Minimum Investment</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>$25,000</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Project Type</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>MultiFamily</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="marketplace-card-button">
+                            <div class="btn full-width">Learn More</div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="col-xs-12 col-sm-4 margin-top-m">
+                    <div class="card">
+                        <div class="marketplace-card-image porperty-image">
+                            <div class="marketplace-card-image-description">
+                                <h5>Mullbery Place Portland, OR</h5>
+                                <p>Mixed Family</p>
+                            </div><img src="/img/img-demo-2.jpg"></div>
+                        <div class="marketplace-card-info">
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Targeted Investor IRR</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>22.8%</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Tareted Equity Multiple </p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>1.8x</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Targeted Invesetment Period</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>3yrs</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Minimum Investment</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>$25,000</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-8">
+                                    <p>Project Type</p>
+                                </div>
+                                <div class="col-xs-12 col-sm-4">
+                                    <p>MultiFamily</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="marketplace-card-button">
+                            <div class="btn full-width">Learn More</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-4 margin-top-m">
                     <div class="card">
                         <div class="marketplace-card-image porperty-image">
                             <div class="marketplace-card-image-description">
@@ -278,61 +383,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-4">
-                    <div class="card">
-                        <div class="marketplace-card-image porperty-image">
-                            <div class="marketplace-card-image-description">
-                                <h5>Mullbery Place Portland, OR</h5>
-                                <p>Mixed Family</p>
-                            </div><img src="/img/img-demo-2.jpg"></div>
-                        <div class="marketplace-card-info">
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Targeted Investor IRR</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>22.8%</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Tareted Equity Multiple </p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>1.8x</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Targeted Invesetment Period</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>3yrs</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Minimum Investment</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>$25,000</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-8">
-                                    <p>Project Type</p>
-                                </div>
-                                <div class="col-xs-12 col-sm-4">
-                                    <p>MultiFamily</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="marketplace-card-button">
-                            <div class="btn full-width">Learn More</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-4">
+                <div class="col-xs-12 col-sm-4 margin-top-m">
                     <div class="card">
                         <div class="marketplace-card-image porperty-image">
                             <div class="marketplace-card-image-description">
@@ -385,8 +436,7 @@
                             <div class="btn full-width">Learn More</div>
                         </div>
                     </div>
-                </div>
-            </div>
+                </div> 
             <div class="row">
                 <div class="col-xs-12"></div>
             </div>
