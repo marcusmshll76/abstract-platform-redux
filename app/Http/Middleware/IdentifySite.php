317be7fab728d5752ec49349532d6b51624ac99b
Benedict Asamoah <wrilben@icloud.com>
2019-07-29 19:58:16 +0000

    identifySite Update

--- app/Http/Middleware/IdentifySite.php
+++ app/Http/Middleware/IdentifySite.php
@@ -12,13 +12,13 @@ class IdentifySite {
         // If we don't find a site, redirect to the first site.
         if (!$maybe_site) {
             $primary = DB::table('sites')->where('id', 1)->first();
-            $a = explode('.', $hostname, 2)[0];
-            if ($a !== 'develop' &&  $a !== 'demo') {
+            $a = explode('.', $hostname, 3);
+            if ($a[0] !== 'develop' &&  $a[0] !== 'demo') {
                 return redirect('http://' . $primary->host);
             } 
             else{
                 $maybe_site = $primary;
-                $maybe_site->host = explode('.', $maybe_site->host, 2)[0];
+                $maybe_site->host = 'abstract';
             }
         } else {
             $maybe_site->host = explode('.', $hostname, 2)[0];
