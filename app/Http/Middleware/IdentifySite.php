ec9050d431246340a1115a90366a0d0fdd770681
Benedict Asamoah <wrilben@icloud.com>
2019-07-30 01:55:30 +0000

    undo middleware changes

--- app/Http/Middleware/IdentifySite.php
+++ app/Http/Middleware/IdentifySite.php
@@ -9,28 +9,12 @@ class IdentifySite {
         $hostname = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
 
         $maybe_site = \DB::table('sites')->where('host', $hostname)->first();
-        $a = explode('.', $hostname, 3);
-        // If we don't find a site.
+        // If we don't find a site, redirect to the first site.
         if (!$maybe_site) {
             $primary = DB::table('sites')->where('id', 1)->first();
-            // check if development server
-            $maybe_site = $primary;
-            if ($a[0] == 'develop') {
-                // Enable sponsor view
-                $maybe_site->host = 'abstract';
-            } elseif ($a[0] != 'demo' && $a[0] != 'develop') {
-                // redirect to the first site
-                return redirect('http://' . $primary->host);
-            }
+            return redirect('http://' . $primary->host);
         } else {
-            // check if development server
-            if ($a[0] == 'develop') {
-                // Enable sponsor view
-                $maybe_site->host = 'abstract';
-            } else {
-                // Set dynamic site host
-                $maybe_site->host = explode('.', $hostname, 2)[0];
-            }
+            $maybe_site->host = explode('.', $hostname, 2)[0];
         }
         
         $request->site = $maybe_site;
